864aa468fee14b3195cfd7fbc68bec40f760b5f5
Wing Yi Valerie So <valeriewyso@gmail.com>
2026-01-06 13:02:50 -0500

    added new .sh files used to do batch jobs to automate 20 trials for baseline numbers

---
+++ submit_automation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=miniMD_energy      # Name of your job
+#SBATCH --output=batch_result_%j.out        # Output file (%j expands to JobID)
+#SBATCH --nodes=1                     # Request 1 physical node
+#SBATCH --ntasks=4                    # Total number of MPI tasks
+#SBATCH --cpus-per-task=1             # Cores per task
+#SBATCH --time=18:00:00               # Max runtime (HH:MM:SS)
+#SBATCH --mem=32G
+#SBATCH --partition=gpu-rgrant          # Name of the queue (check your cluster docs)
+#SBATCH --mail-user=20wyvs@queensu.ca
+#SBATCH --mail-type=END,FAIL
+
+# Run the automation script
+chmod +x automated_runs.sh
+./automated_runs.sh
